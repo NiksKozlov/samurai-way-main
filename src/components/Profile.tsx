1166eb2031b5dd47b9190dbe28f868b8c5b6c2df
NiksKozlov <nikskozlov@gmail.com>
2022-08-18 23:51:25 +0300

    Add empty CSS modules for Header, Navbar, Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,10 +7,7 @@ const Profile = () => {
                 <img src='https://html5css.ru/css/img_forest.jpg' alt='img'/>
             </div>
             <div>
-                <img
-                    src='https://media.npr.org/assets/img/2021/08/06/dog-4415649-18eab39206426b985f7a5f69e3146a2cd1a56c0d-s1100-c50.jpg'
-                    alt='img'/>
-                description
+                description + ava
             </div>
             <div>
                 My posts
