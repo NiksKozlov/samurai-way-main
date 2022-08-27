210a2ccd57153bf113616dda0819bb7d142646f3
NiksKozlov <nikskozlov@gmail.com>
2022-08-28 01:59:48 +0300

    Message and user templates have been added to the dialog component. Minimal CSS in Dialogs Module

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={` ${s.dialog} ${s.active}`}>
+                    Nikita
+                </div>
+                <div className={s.dialog}>
+                    Alina
+                </div>
+                <div className={s.dialog}>
+                    Mark
+                </div>
+                <div className={s.dialog}>
+                    Pavel
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     )
 }
