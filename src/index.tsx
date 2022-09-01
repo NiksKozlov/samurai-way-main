b844b7b756957db6a3728902c4b7dfcc1bba420f
NiksKozlov <nikskozlov@gmail.com>
2022-09-01 05:22:20 +0300

    Move posts data to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+let posts = [
+    {id: 1, message: "Hi! How are you?", likesCount: "15"},
+    {id: 2, message: "It's my first post!!!", likesCount: "20"},
+]
+
+
+
+
 ReactDOM.render(
     <App />,
   document.getElementById("root")
